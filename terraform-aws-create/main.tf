2845657c32e5ee0b95f3df655e0435947a49f97a
Arif Sadiq <38719031+arifsadiq@users.noreply.github.com>
2024-07-06 20:31:03 +0400

    Update main.tf

--- terraform-aws-create/main.tf
+++ terraform-aws-create/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.26.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-  }
-
 resource "aws_instance" "SeverProtege" {
   ami = var.instance_ami
   instance_type = var.ec2_type
